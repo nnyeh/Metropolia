2aa67760a47ae446e6c44c45fa5da9b7fc1403c2
nnyeh <38233746+nnyeh@users.noreply.github.com>
2022-09-21 01:03:12 +0300

    Create Main.java

---
+++ Suunnittelumallit/Tehtävä 2: Abstract Factory/Main.java
@@ -0,0 +1,15 @@
+package tsydeemi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Vaatevalinta vaatteet = new Vaatteet();
+		
+		vaatteet.luoFarmarit("Adidas");
+		vaatteet.luoPaita("Adidas");
+		vaatteet.luoLippis("Adidas");
+		vaatteet.luoKengät("Adidas");
+
+	}
+}

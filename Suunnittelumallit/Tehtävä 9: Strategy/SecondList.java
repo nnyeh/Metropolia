522a7aabdb7b31ff7faa629e367aeccbbe6d61eb
nnyeh <38233746+nnyeh@users.noreply.github.com>
2022-10-13 03:38:43 +0300

    Add files via upload

---
+++ Suunnittelumallit/Tehtävä 9: Strategy/SecondList.java
@@ -0,0 +1,28 @@
+package tsydeemi;
+
+import java.util.List;
+
+public class SecondList implements ListConverter {
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public String listToString(List list) {
+		
+        String stringList = "";
+        String tempList[] = new String[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+        	tempList[i] = list.get(i).toString();
+        }
+
+        for (int i = 1; i < list.size() + 1; i++) {
+        	stringList += tempList[i - 1];
+            if(i % 2 == 0) {
+            	stringList += "\n";
+            }
+        }
+        
+        return stringList;
+        
+    }
+}

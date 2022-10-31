a9945633a80801a832fbf2285e0642ec1acf59f7
nnyeh <38233746+nnyeh@users.noreply.github.com>
2022-10-31 20:39:20 +0200

    ‎

---
+++ Suunnittelumallit/Tehtävä 11: Memento/Main.java
@@ -0,0 +1,16 @@
+package tsydeemi;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Randomizer p1 = new Randomizer("Emil");
+		Randomizer p2 = new Randomizer("Kim");
+		Randomizer p3 = new Randomizer("Petteri");
+		
+		p1.start();
+		p2.start();
+		p3.start();
+
+	}
+}

2e76cdaecbfb550196f4b6b085bd434050d545d7
nnyeh <38233746+nnyeh@users.noreply.github.com>
2022-12-11 02:56:33 +0200

    ‎

---
+++ Suunnittelumallit/Tehtävä 19: Mediator/b/Thread1.java
@@ -0,0 +1,16 @@
+package tsydeemi;
+
+public class Thread1 extends Thread {
+
+	NumberList numberlist;
+	
+	public Thread1(NumberList numberlist) {
+		this.numberlist = numberlist;
+	}	 
+
+	@Override
+	public void run() {
+		numberlist.iterate();
+	}
+	
+}
